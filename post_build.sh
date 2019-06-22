7f2b64f9956d511207c70d26459280aaf16347f3
mrz1836 <mrz1818@gmail.com>
2019-06-22 13:35:14 -0400

    Copy other static resources to build directory

--- post_build.sh
+++ post_build.sh
@@ -2,3 +2,8 @@ cp -R ./node_modules/bsv-satchel ./build/node_modules/bsv-satchel
 cp ./vendor/prism/prism.js ./build/js/prism.js
 cp ./vendor/prism/prism.css ./build/css/prism.css
 cp ./node_modules/clipboard/dist/clipboard.min.js ./build/js/clipboard.min.js
+cp ./src/favicon.ico ./build/favicon.ico
+cp ./src/browserconfig.xml ./build/browserconfig.xml
+cp ./src/ror.xml ./build/ror.xml
+cp ./src/site.webmanifest ./build/site.webmanifest
+cp ./src/sitemap.xml ./build/sitemap.xml
